3f80e764cf3b330561161474e73b2c9813bfd8de
jamie ingalls <jamesmaclay@gmail.com>
2025-03-12 17:57:04 -0400

    image assests added, button ui added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 
-setInterval(() => {
-  const backgroundColors = ['#0d1b2a', '#212529', '#323232', '#353530', '#495057', '#323232', '#0d1b2a', '#343a40','#313131','#495057','#212529','#0d1b2a','#323232','#353530','#000000','#343a40'];
-  const randomIndex = Math.floor(Math.random() * backgroundColors.length);
-  body.style.backgroundColor = backgroundColors[randomIndex];
-}, 10000);
+// setInterval(() => {
+//   const backgroundColors = ['#0d1b2a', '#212529', '#323232', '#353530', '#495057', '#323232', '#0d1b2a', '#343a40','#313131','#495057','#212529','#0d1b2a','#323232','#353530','#000000','#343a40'];
+//   const randomIndex = Math.floor(Math.random() * backgroundColors.length);
+//   body.style.backgroundColor = backgroundColors[randomIndex];
+// }, 10000);
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 function App() {
 
   return (
     <>
-     <h1>my portfolio</h1>
+     <Button variant="default">Click Me</Button>;
     </>
   )
 }
